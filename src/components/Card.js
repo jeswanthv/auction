e29d707875dd77824c279db30fc9172331e9d875
jeswanthv <jeswanthv001@gmail.com>
2022-03-09 14:47:03 +0530

    format

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,15 @@
 import React from 'react';
+const inrformat = require('inrformat');
 
 function Card({ name, budget, photo }) {
+  let inr = inrformat(budget);
   return (
     <div className='flex flex-col justify-center items-center'>
       <img class='rounded-lg h-28 w-28 -mb-10 z-10 ' src={photo} alt={name} />
       <div class='flex justify-center  p-2 rounded-lg'>
         <div class='flex justify-center flex-col items-center  shadow-lg  bg-gray-300 rounded-b-lg  max-w-sm p-2'>
           <div class='p-5 text-center '>
-            <p class='text-gray-900 font-semibold text-3xl mb-4'>{budget}</p>
+            <p class='text-gray-900 font-semibold text-3xl mb-4'>{inr}</p>
           </div>
         </div>
       </div>
